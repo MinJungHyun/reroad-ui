bd7c0061c60c0d4b9283dca366b79230bcb56b2e
MinJungHyun__ <misookhe@gmail.com>
2023-10-29 11:31:02 +0900

    [#1] 레이아웃 푸터 정리
    Fixes #1
    
    - 레이아웃 푸터 정리

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,18 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
-  return 'main';
+  const LoginKakaoButton = () => {
+    return (
+      <Link href="http://localhost:3010/auth/kakao">
+        <button>카카오로그인</button>
+      </Link>
+    );
+  };
+  return (
+    <div>
+      <LoginKakaoButton></LoginKakaoButton>
+    </div>
+  );
 }
 
