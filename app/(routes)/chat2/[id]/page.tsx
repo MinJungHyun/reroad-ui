46bc3b5682907b62b3a3d847fffa5969408fd43f
MinJungHyunA <misookhe@gmail.com>
2024-04-29 19:01:57 +0900

    채팅 메시지 전달

--- app/(routes)/chat2/[id]/page.tsx
+++ app/(routes)/chat2/[id]/page.tsx
@@ -1,34 +1,61 @@
-// pages/index.js
-
+'use client';
 import { useEffect, useState } from 'react';
 import io, { Socket } from 'socket.io-client';
-
-export default function Home() {
+interface Props {
+  id: string;
+}
+interface IMessageItem {
+  type: 'string' | 'image';
+  message: string;
+  roomId: string;
+  userId: number;
+}
+export default function Home({ id }: Props) {
+  const [roomId, setRoomId] = useState<string>('100');
+  const [userId, setUserId] = useState<number>(1);
   const [socket, setSocket] = useState<Socket>();
-  const [messages, setMessages] = useState([]);
+  const [messageItems, setMessageItems] = useState<IMessageItem[]>([]);
   const [input, setInput] = useState('');
 
+  const makeMessageObj = (message: string) => {
+    return {
+      type: 'string',
+      message: message,
+      roomId: roomId,
+      userId: userId
+    };
+  };
+
   useEffect(() => {
-    const newSocket = io('http://localhost:3010'); // NestJS 서버의 주소에 맞게 변경해야 합니다.
+    if (id) {
+      setRoomId(id);
+    }
+  }, [id]);
+
+  useEffect(() => {
+    const newSocket = io('http://localhost:3010', { query: { roomId } }); // NestJS 서버의 주소에 맞게 변경해야 합니다.
     setSocket(newSocket);
 
     newSocket.on('connect', () => {
       console.log('Connected to server');
     });
 
-    newSocket.on('chatMessage', (message: any) => {
-      setMessages((prevMessages) => [...prevMessages, message]);
+    newSocket.on('chatMessage', (message: IMessageItem) => {
+      console.log('@@@@ front ', message);
+
+      setMessageItems((prevMessages) => [...prevMessages, message]);
     });
 
     return () => {
       newSocket.disconnect();
+      console.log('Disconnected from server');
     };
   }, []);
 
   const sendMessage = () => {
     if (input.trim() !== '') {
       if (socket) {
-        socket.emit('chatMessage', input);
+        socket.emit('chatMessage', makeMessageObj(input));
         setInput('');
       }
     }
@@ -38,10 +65,16 @@ export default function Home() {
     <div>
       <h1>Chat App</h1>
       <div>
-        {messages.map((message, index) => (
-          <p key={index}>{message}</p>
-        ))}
+        {messageItems.map((messageItem, index) => {
+          const { userId, message } = messageItem;
+          return (
+            <p key={index}>
+              {[userId]} {message}
+            </p>
+          );
+        })}
       </div>
+
       <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Type your message..." />
       <button onClick={sendMessage}>Send</button>
     </div>
